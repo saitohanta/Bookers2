0eca92fe863ddce7c445aaa63a980c2e4bd8b2bf
saitohanta <osyogatu17@icloud.com>
2023-07-29 01:38:45 +0000

    [Add]

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,12 @@
 class Book < ApplicationRecord
-  
-  
   belongs_to :user
+  has_many :favorites, dependent: :destroy
+
   validates :title, presence: true
   validates :body, length: { in: 1..200 }
 
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
-  resources :users, only: [:index, :show, :edit, :update]
-  get "/home/about" => "homes#about", as: "about"
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
+  get "/home/about" => "homes#about", as: "about"
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+  end
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
